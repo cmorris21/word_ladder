b6f558c413cec5057f007418e4e24a44bdbc4974
cmorris21 <54951716+cmorris21@users.noreply.github.com>
2020-02-21 18:48:17 -0600

    Update word_ladder.py

--- word_ladder.py
+++ word_ladder.py
@@ -1,13 +1,30 @@
 #!/bin/python3
 from collections import deque
 from copy import deepcopy
+'''
+Create a stack
+Push the start word onto the stack
+Create a queue
+Enqueue the stack onto the queue
 
+While the queue is not empty
+    Dequeue a stack from the queue
+    For each word in the dictionary
+        If the word is adjacent to the top of the stack
+            If this word is the end word
+                You are done!
+                The front stack plus this word is your word ladder.
+            Make a copy of the stack
+            Push the found word onto the copy
+            Enqueue the copy
+            Delete word from the dictionary
+'''
 
 def word_ladder(start_word, end_word, dictionary_file='words5.dict'):
     
     if start_word == end_word:
         return [start_word]
-    
+        
     stack = []
     stack.append(start_word)
     que = deque()
@@ -17,20 +34,21 @@ def word_ladder(start_word, end_word, dictionary_file='words5.dict'):
     words = []
 
     for x in word_file:
-        words.append(x.strip("\n"))
-   
-    while len(que) > 0:
-        que.pop()
+        words.appendleft(x.strip("\n"))
+    while len(que) != 0:
+        top = que.pop()
         for word in words:
-            if _adjacent(word,stack[0]): 
-                if word == end_word:          
-                    stack.append(word)
-                    return stack
-                copylist = deepcopy(stack)
+            remove = []
+            if _adjacent(word,top[-1]): 
+                copylist = copy.deepcopy(top)
                 copylist.append(word)
-                que.appendleft(copyList)
+                if word == end_word:
+                    for i in range(1,len(copylist)-2):
+                        if _adjacent(top[i-1],top[i+1]):
+                            copylist.pop(i)
+                    return (copylist)
+                ourQ.appendleft(copylist)
                 words.remove(word)
-    return None
  
     '''
     Returns a list satisfying the following properties:
@@ -83,26 +101,16 @@ def _adjacent(word1, word2):
     >>> _adjacent('stone','money')
     False
     '''
-    
-    if len(word1)== len(word2):  #checks if both words are the same length
-        numdifs = 0              #assuming there are no differences between the two words   
-        for x,y in zip(word1, word2):      #pairing the words one for one and seeing if they're similar
-            if x!= y:              #if they're not similar, we increment numdifs by one
-                if numdifs:         #if numdifs is 1 or greater, they're not adjacent
-                    return False
-                numdifs +=1
+    if word1 == word2:
+        return False
+    if len(word1)==len(word2):
+        count = 0
+        for i in range(len(word1)):
+            if word1[i] == word2[i]:
+                count += 1
+    if count == 3:
         return True
     else:
         return False
-    
-    #if len(word1)==len(word2):
-     #   count = 0
-      #  for i in range(len(word1)):
-       #     if word1[i] == word2[i]:
-        #        count += 1
-    #if count == 3:
-     #   return True
-    #else:
-     #   return False
       
             
